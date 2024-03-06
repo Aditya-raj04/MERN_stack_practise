c5fbfc1a5484778ad86bf87e1c52e1d8dd59e6d6
Sh4dow_3cho <117041323+Aditya-raj04@users.noreply.github.com>
2024-03-06 15:49:02 +0000

    datatypes

---
+++ basics/1_datatypes.js
@@ -0,0 +1,15 @@
+console.log("aditya");
+"use strict";
+
+//numbers
+//strings
+//bigInt
+//undefined
+//null
+//boolean
+//symbol
+
+//object
+
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
