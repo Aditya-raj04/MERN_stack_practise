118ae611654290efd237d19aa31d6d47aa4ba36b
Sh4dow_3cho <117041323+Aditya-raj04@users.noreply.github.com>
2024-03-06 16:28:14 +0000

    conversion of data types

--- basics/1_datatypes.js
+++ basics/1_datatypes.js
@@ -13,3 +13,16 @@ console.log("aditya");
 
 console.log(typeof undefined)//undefined
 console.log(typeof null)//object
+
+let num = "123abc";
+
+console.log(typeof Number(num));
+console.log(typeof String(num));
+console.log(Number(num));
+console.log(String(num));
+
+
+//"33" > number
+//"33abc" > NaN
+//true:1 ; false:0
+//
