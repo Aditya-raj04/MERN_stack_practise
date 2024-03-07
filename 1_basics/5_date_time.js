b93fca2895d3df7351308a8f19f31256ecac694a
Sh4dow_3cho <117041323+Aditya-raj04@users.noreply.github.com>
2024-03-07 16:37:59 +0000

    basic done

---
+++ 1_basics/5_date_time.js
@@ -0,0 +1,42 @@
+//dates
+// 1
+let myDate = new Date();
+// console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleString())
+// console.log(myDate);
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleTimeString())
+// console.log(typeof myDate)
+
+// 2
+let myNewDate = new Date("01-14-2023")
+// let myNewDate = new Date(2023,0,31)
+// let myNewDate = new Date("2023-01-24")
+// console.log(myNewDate.toLocaleString())
+
+
+// 3
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myNewDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+// 4
+let myOtherDate = new Date()
+// console.log(myOtherDate.getDate())
+// console.log(myOtherDate.getMonth() + 1)
+// console.log(myOtherDate.getFullYear())
+
+// console.log(`the date is "${myOtherDate.getDate()}" month is "${myOtherDate.getMonth() + 1}" year is "${myOtherDate.getFullYear()}"`)
+
+
+
+// 5
+myNewDate.toLocaleString('default', {
+    weekday:"narrow",
+    day:"2-digit",
+    timeZoneName:"short",
+})
+console.log(myNewDate)
