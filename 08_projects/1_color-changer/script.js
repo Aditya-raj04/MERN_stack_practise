78b375bcdfb03331ed45762b66da4c060cbbbf89
Sh4dow_3cho <117041323+Aditya-raj04@users.noreply.github.com>
2024-05-27 15:11:00 +0000

    dom project 1

--- 08_projects/1_color-changer/script.js
+++ 08_projects/1_color-changer/script.js
@@ -54,28 +54,41 @@
 
 
 
-const buttons = document.querySelectorAll(".button")
-// console.log(buttons);
+// const buttons = document.querySelectorAll(".button")
+// // console.log(buttons);
 
-const body = document.querySelector("body");
-buttons.forEach( (btn) =>{
-    console.log(btn);
-    btn.addEventListener('click' , function(e){
-if(e.target.id=="grey"){
-    body.style.backgroundColor=e.target.id;
-}
-if(e.target.id=="white"){
-    body.style.backgroundColor=e.target.id;
-}
-if(e.target.id=="yellow"){
-    body.style.backgroundColor=e.target.id;
-}
-if(e.target.id=="blue"){
-    body.style.backgroundColor=e.target.id;
-}
-if(e.target.id=="purple"){
-    body.style.backgroundColor=e.target.id;
-}
-    })
+// const body = document.querySelector("body");
+// buttons.forEach( (btn) =>{
+//     console.log(btn);
+//     btn.addEventListener('click' , function(e){
+// if(e.target.id=="grey"){
+//     body.style.backgroundColor=e.target.id;
+// }
+// if(e.target.id=="white"){
+//     body.style.backgroundColor=e.target.id;
+// }
+// if(e.target.id=="yellow"){
+//     body.style.backgroundColor=e.target.id;
+// }
+// if(e.target.id=="blue"){
+//     body.style.backgroundColor=e.target.id;
+// }
+// if(e.target.id=="purple"){
+//     body.style.backgroundColor=e.target.id;
+// }
+//     })
 
-} );
+// } );
+
+
+
+
+
+
+const buttons = document.querySelectorAll(".button");
+      const body = document.querySelector("body");
+      buttons.forEach((button) => {
+        button.addEventListener("click", (e) => {
+          body.style.backgroundColor = e.target.id;
+        });
+      });
