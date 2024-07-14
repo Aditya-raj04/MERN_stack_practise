7bde46e4496e6e44e6209fb5a6fe49dc7fe36859
Sh4dow_3cho <117041323+Aditya-raj04@users.noreply.github.com>
2024-07-14 18:58:27 +0000

    call

---
+++ 11_classes_and_oops/4_call.js
@@ -0,0 +1,17 @@
+function setusername(username){
+    //complex db calls
+    this.username = username;
+    console.log("called")
+    
+}
+
+function createuser(username,email,password){
+    setusername.call(this, username);    //call method
+    this.password=password;
+    this.email=email;
+    // this.username=username;
+}
+
+
+const chai = new createuser("chai","chai.com",123);
+console.log(chai);
