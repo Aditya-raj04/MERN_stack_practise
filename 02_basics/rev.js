038c376fcb59c93744b451d249e951e6e60cf5bd
Sh4dow_3cho <117041323+Aditya-raj04@users.noreply.github.com>
2024-11-18 12:11:46 +0000

    revision

--- 02_basics/rev.js
+++ 02_basics/rev.js
@@ -11,3 +11,29 @@ let x = 5; // Initial value of x
 jsuser.greetingsTwo(x); // Call the greetingsTwo function
 
 console.log("Outside function x:", x); // Output: 5 (Original x remains unchanged)
+
+
+
+
+//function
+
+const newuser = {
+    useername: "aditya",
+    prices: 999
+}
+
+function enduser(anyobject){
+    console.log(`the username is ${anyobject.useername} and the price is ${anyobject.prices}`)
+}
+
+// enduser(newuser);
+// enduser({
+//     useername:"aditya",
+//     prices:875
+// })
+
+
+
+
+
+
