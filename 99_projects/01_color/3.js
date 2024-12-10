15feda99e407a177c3f7c2ea6c4ccae36ee45cd0
Sh4dow_3cho <117041323+Aditya-raj04@users.noreply.github.com>
2024-12-10 12:10:36 +0000

    events

--- 99_projects/01_color/3.js
+++ 99_projects/01_color/3.js
@@ -9,11 +9,26 @@
 // });
 
 
-const btn = document.querySelectorAll(".button")
+// const btn = document.querySelectorAll(".button")
 
-const body = document.querySelector("body")
-for(let i = 0 ; i <= btn.length; i++){
-    btn[i].addEventListener('click' , (e)=>{
-        body.style.backgroundColor = e.target.id
-    })
-}
+// const body = document.querySelector("body")
+// for(let i = 0 ; i <= btn.length; i++){
+//     btn[i].addEventListener('click' , (e)=>{
+//         body.style.backgroundColor = e.target.id
+//     })
+// }
+
+
+const buttons = document.querySelectorAll(".button"); // Selects all <button> elements
+const body = document.querySelector("body"); // Selects the <body> element
+
+buttons.forEach((btn) => {
+  console.log(btn); // Logs each button to the console
+  btn.addEventListener("click",(e)=>{
+    console.log(e)
+    console.log(e.target.id)
+    body.style.backgroundColor = e.target.id
+  })
+});
+
+
